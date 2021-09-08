bb776150934bafd880bfd71569ffbc2887b3f2ce
Brian Best <bebest@att.net>
2021-09-08 17:50:21 -0400

    Add project files.

---
+++ Program 2b/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program_2b
+// ISM 4300 Program #2b - Created by Brian Best
+// This program gives the user what grade they will receive with their current score.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What score do you currently have in the course?");
+        }
+    }
+}
